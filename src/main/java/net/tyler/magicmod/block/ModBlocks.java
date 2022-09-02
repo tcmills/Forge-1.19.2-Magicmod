d6c3fbf0bd3e9346c45b35051c241b9d40bae333
tcmills <tcmills@vt.edu>
2022-09-01 20:08:53 -0400

    Added Mana Crystal Block, Ore, and Deepslate Ore

---
+++ src/main/java/net/tyler/magicmod/block/ModBlocks.java
@@ -0,0 +1,49 @@
+package net.tyler.magicmod.block;
+
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.tyler.magicmod.MagicMod;
+import net.tyler.magicmod.item.ModCreativeModeTab;
+import net.tyler.magicmod.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, MagicMod.MOD_ID);
+
+    public static final RegistryObject<Block> MANA_CRYSTAL_BLOCK = registerBlock("mana_crystal_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+            .strength(0.25f).requiresCorrectToolForDrops()), ModCreativeModeTab.MAGIC_TAB);
+    public static final RegistryObject<Block> MANA_CRYSTAL_ORE = registerBlock("mana_crystal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+            .strength(0.15f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.MAGIC_TAB);
+    public static final RegistryObject<Block> DEEPSLATE_MANA_CRYSTAL_ORE = registerBlock("deepslate_mana_crystal_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(0.225f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.MAGIC_TAB);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
