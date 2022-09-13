8532a7b56b2c8a4ace9f3b6150db9eb077ce16ff
tcmills <tcmills@vt.edu>
2022-09-12 21:34:33 -0400

    Added Mana Distiller

--- src/main/java/net/tyler/magicmod/block/ModBlocks.java
+++ src/main/java/net/tyler/magicmod/block/ModBlocks.java
@@ -16,6 +16,7 @@ import net.minecraftforge.registries.RegistryObject;
 import net.tyler.magicmod.MagicMod;
 import net.tyler.magicmod.block.custom.JumpyBlock;
 import net.tyler.magicmod.block.custom.ManaCrystalLampBlock;
+import net.tyler.magicmod.block.custom.ManaDistiller;
 import net.tyler.magicmod.item.ModCreativeModeTab;
 import net.tyler.magicmod.item.ModItems;
 
@@ -27,13 +28,13 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> MANA_CRYSTAL_BLOCK = registerBlock("mana_crystal_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-            .strength(5f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST).lightLevel(state -> 6)), ModCreativeModeTab.MAGIC_TAB);
+            .strength(5f, 6f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST).lightLevel(state -> 6)), ModCreativeModeTab.MAGIC_TAB);
     public static final RegistryObject<Block> MANA_CRYSTAL_ORE = registerBlock("mana_crystal_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
-            .strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.MAGIC_TAB);
+            .strength(3f, 3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.MAGIC_TAB);
     public static final RegistryObject<Block> DEEPSLATE_MANA_CRYSTAL_ORE = registerBlock("deepslate_mana_crystal_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(4.5f).requiresCorrectToolForDrops()
+                    .strength(4.5f, 3f).requiresCorrectToolForDrops()
                     .sound(SoundType.DEEPSLATE),
                     UniformInt.of(3, 7)), ModCreativeModeTab.MAGIC_TAB);
 
@@ -46,7 +47,12 @@ public class ModBlocks {
                     .strength(-1.0f, 3600000.0F).sound(SoundType.SLIME_BLOCK)), ModCreativeModeTab.MAGIC_TAB);
     public static final RegistryObject<Block> MANA_CRYSTAL_LAMP = registerBlock("mana_crystal_lamp",
             () -> new ManaCrystalLampBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(0.3f).sound(SoundType.GLASS).lightLevel(state -> state.getValue(ManaCrystalLampBlock.LIT) ? 0 : 15)), ModCreativeModeTab.MAGIC_TAB);
+                    .strength(0.3f, 0.3f).sound(SoundType.GLASS).lightLevel(state -> state.getValue(ManaCrystalLampBlock.LIT) ? 0 : 15)), ModCreativeModeTab.MAGIC_TAB);
+
+    public static final RegistryObject<Block> MANA_DISTILLER = registerBlock("mana_distiller",
+            () -> new ManaDistiller(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(3.5f, 3.5f).requiresCorrectToolForDrops().noOcclusion()
+                    .lightLevel(state -> 6)), ModCreativeModeTab.MAGIC_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
