cc2685d29d11a53d6f76ec6e64f39cd8cae6a7b8
tcmills <tcmills@vt.edu>
2023-08-04 21:27:35 -0400

    Fishing does not generate enchanted items now

--- src/main/java/net/tyler/magicmod/event/loot/MagicFromFishingAdditionModifier.java
+++ src/main/java/net/tyler/magicmod/event/loot/MagicFromFishingAdditionModifier.java
@@ -1,31 +1,26 @@
 package net.tyler.magicmod.event.loot;
 
 import com.google.common.base.Suppliers;
-import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.GsonHelper;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.*;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.tyler.magicmod.capability.info.PlayerInfoProvider;
-import net.tyler.magicmod.client.ClientInfoData;
-import net.tyler.magicmod.item.ModItems;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class MagicFromFishingAdditionModifier extends LootModifier {
@@ -46,6 +41,14 @@ public class MagicFromFishingAdditionModifier extends LootModifier {
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
 
         if (context.getParam(LootContextParams.KILLER_ENTITY) instanceof Player player) {
+
+            Map<Enchantment, Integer> enchant = EnchantmentHelper.getEnchantments(generatedLoot.get(0));
+            if (!enchant.isEmpty()) {
+                generatedLoot.clear();
+                generatedLoot.add(new ItemStack(Items.COD));
+                player.sendSystemMessage(Component.literal("Enchanted").withStyle(ChatFormatting.YELLOW));
+            }
+
             player.getCapability(PlayerInfoProvider.PLAYER_INFO).ifPresent(info -> {
                 if (info.getWater()) {
                     LootTable extraTable = context.getLootTable(this.lootTableId);
